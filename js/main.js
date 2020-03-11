b18bbf8eaf855f9137d351a884ecb50c5f8e8b4d
Eden <gardenofeden12@gmail.com>
2020-03-10 20:35:05 -0400

    worked on javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 (function () {
 
-  // your js goes here
+  var buttonOne = document.querySelector('button');
+
+  var buttonTwo = document.querySelector('.button-success')
+
+  var buttonThree = document.querySelector('.button-warning')
+
+  document.addEventListener('click', function(event) {
+
+    if (event.target === buttonOne) {
+
+       buttonOne.classList.toggle('button-danger');
+    }
+    if (event.target === buttonTwo) {
+
+      buttonTwo.innerText = 'You clicked Me!';
+    }
+
+    if (event.target === buttonThree) {
+
+      buttonTwo.innerText = 'button two';
+    }
+  });
 
 })();
